3ab5730a0509d9dd28031ca75293c596d09fa0fb
vvvbbbcz <vvvbbbcz@163.com>
2022-12-29 22:00:12 +0800

    补全 package-info.java

---
+++ src/main/java/cn/bzgzs/industrybase/event/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package cn.bzgzs.industrybase.event;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ src/main/java/cn/bzgzs/industrybase/network/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package cn.bzgzs.industrybase.network;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;

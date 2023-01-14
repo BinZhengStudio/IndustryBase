ea4f43c392a095aae4bdc837245431e7a2137eb7
vvvbbbcz <vvvbbbcz@163.com>
2023-01-15 00:12:46 +0800

    导线下垂效果改进

--- src/main/java/cn/bzgzs/industrybase/client/renderer/blockentity/WireConnectorRenderer.java
+++ src/main/java/cn/bzgzs/industrybase/client/renderer/blockentity/WireConnectorRenderer.java
@@ -50,8 +50,9 @@ public class WireConnectorRenderer implements BlockEntityRenderer<WireConnectorB
 		float totalZ = (float) (end.z - start.z);
 		VertexConsumer consumer = bufferSource.getBuffer(RenderType.leash());
 		Matrix4f matrix4f = poseStack.last().pose();
-		float width = 0.05F; // 水平方向大小
+		float width = 0.05F; // 导线粗细
 		float square = totalX * totalX + totalZ * totalZ;
+		float horizonDistance = Mth.sqrt(totalX * totalX + totalZ * totalZ);
 		boolean vertical = square == 0;
 		// 在添加顶点时，相同宽度会被添加两次，因此需要除以 2
 		float size = Mth.fastInvSqrt(square) * width / 2.0F;
@@ -67,13 +68,13 @@ public class WireConnectorRenderer implements BlockEntityRenderer<WireConnectorB
 		int toSkyLight = level.getBrightness(LightLayer.SKY, to);
 
 		for (int i = 0; i <= 24; ++i) { // 一共 25 段
-			addVertexPair(consumer, matrix4f, totalX, totalY, totalZ, fromLightLevel, toLightLevel, fromSkyLight,
+			addVertexPair(consumer, matrix4f, totalX, totalY, totalZ, horizonDistance, fromLightLevel, toLightLevel, fromSkyLight,
 					toSkyLight, widthY, widthX, widthZ, i);
 		}
 
 		for (int j = 24; j >= 0; --j) { // 再来一次，以渲染两个相互交叉的面
 			// 需要将 widthY 取相反数，否则生成的面与原有面重合
-			addVertexPair(consumer, matrix4f, totalX, totalY, totalZ, fromLightLevel, toLightLevel, fromSkyLight,
+			addVertexPair(consumer, matrix4f, totalX, totalY, totalZ, horizonDistance, fromLightLevel, toLightLevel, fromSkyLight,
 					toSkyLight, -widthY, vertical ? -widthX : widthX, widthZ, j);
 		}
 
@@ -81,7 +82,7 @@ public class WireConnectorRenderer implements BlockEntityRenderer<WireConnectorB
 	}
 
 	private static void addVertexPair(VertexConsumer consumer, Matrix4f matrix4f, float totalX, float totalY,
-									  float totalZ, int fromLightLevel, int toLightLevel, int fromSkyLight,
+									  float totalZ, float horizonDistance, int fromLightLevel, int toLightLevel, int fromSkyLight,
 									  int toSkyLight, float widthY, float widthX, float widthZ, int index) {
 		float delta = (float) index / 24.0F;
 		int lightLevel = (int) Mth.lerp(delta, (float) fromLightLevel, (float) toLightLevel);
@@ -90,8 +91,14 @@ public class WireConnectorRenderer implements BlockEntityRenderer<WireConnectorB
 		float red = 0.1F;
 		float green = 0.1F;
 		float blue = 0.1F;
-		float x = totalX * delta;
+		float f = 0.08F; // 下垂量，经测试，0.08 下垂较为自然
+		// 用的是二次函数
 		float y = totalY > 0.0F ? totalY * delta * delta : totalY - totalY * (1.0F - delta) * (1.0F - delta);
+		// 增加下垂量，并使水平连接的导线也有下垂效果
+		y += delta > 0.5F ? horizonDistance * f * (delta - 0.5F) * (delta - 0.5F) : horizonDistance * f * (0.5F - delta) * (0.5F - delta);
+		// 纠正导线位置
+		y -= horizonDistance * f * 0.5F * 0.5F;
+		float x = totalX * delta;
 		float z = totalZ * delta;
 		consumer.vertex(matrix4f, x - widthX, y + widthY, z + widthZ).color(red, green, blue, 1.0F).uv2(packedLight).endVertex();
 		consumer.vertex(matrix4f, x + widthX, y - widthY, z - widthZ).color(red, green, blue, 1.0F).uv2(packedLight).endVertex();

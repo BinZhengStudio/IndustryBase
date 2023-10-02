6ca994b94ad0ca4b49146ebc2ff3a323633bdfcd
vvvbbbcz <vvvbbbcz@bzgzs.cn>
2023-10-02 16:27:17 +0800

    简单的搞了一下流体渲染

--- src/main/java/cn/bzgzs/industrybase/client/renderer/blockentity/CreativeSteamEngineRenderer.java
+++ src/main/java/cn/bzgzs/industrybase/client/renderer/blockentity/CreativeSteamEngineRenderer.java
@@ -3,14 +3,13 @@ package cn.bzgzs.industrybase.client.renderer.blockentity;
 import cn.bzgzs.industrybase.api.IndustryBaseApi;
 import cn.bzgzs.industrybase.world.level.block.entity.CreativeSteamEngineBlockEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
-import net.minecraftforge.common.ForgeMod;
+import org.joml.Matrix4f;
 
 public class CreativeSteamEngineRenderer implements BlockEntityRenderer<CreativeSteamEngineBlockEntity> {
 	private static final ResourceLocation TEXTURE = new ResourceLocation(IndustryBaseApi.MODID, "textures/entity/fake_fluid.png");
@@ -23,18 +22,13 @@ public class CreativeSteamEngineRenderer implements BlockEntityRenderer<Creative
 
 	@Override
 	public void render(CreativeSteamEngineBlockEntity blockEntity, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, int packedOverlay) {
-		// 渲染一个透明小立方体，代替水
 		poseStack.pushPose();
-		// 使透明立方体居中并比透明容器略小
-		poseStack.translate(0.06252D, 0.5001D, 0.06252D);
-		poseStack.scale(0.8746F, 0.4365F, 0.8746F);
-		int fluidColor = IClientFluidTypeExtensions.of(ForgeMod.WATER_TYPE.get()).getTintColor(); // 获取水的颜色
-		float red = (float)(fluidColor >> 16 & 255) / 255.0F;
-		float green = (float)(fluidColor >> 8 & 255) / 255.0F;
-		float blue = (float)(fluidColor & 255) / 255.0F;
-		float alpha = (float)(fluidColor >> 24 & 255) / 255.0F;
-		// 执行渲染
-		this.main.render(poseStack, bufferSource.getBuffer(RenderType.entityTranslucent(TEXTURE)), packedLight, packedOverlay, red, green, blue, alpha);
+		SteamEngineRenderer.renderWater(1.0F, blockEntity, poseStack, bufferSource, packedLight);
 		poseStack.popPose();
 	}
+
+	private void vertex(VertexConsumer consumer, Matrix4f matrix4f, float pX, float pY, float pZ, float pRed, float pGreen, float pBlue, float alpha, float pU, float pV, int pPackedLight) {
+		consumer.vertex(matrix4f, pX, pY, pZ).color(pRed, pGreen, pBlue, alpha).uv(pU, pV).uv2(pPackedLight).normal(0.0F, 1.0F, 0.0F).endVertex();
+	}
+
 }
--- src/main/java/cn/bzgzs/industrybase/client/renderer/blockentity/SteamEngineRenderer.java
+++ src/main/java/cn/bzgzs/industrybase/client/renderer/blockentity/SteamEngineRenderer.java
@@ -2,20 +2,24 @@ package cn.bzgzs.industrybase.client.renderer.blockentity;
 
 import cn.bzgzs.industrybase.api.IndustryBaseApi;
 import cn.bzgzs.industrybase.world.level.block.entity.SteamEngineBlockEntity;
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.*;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
-import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.material.FlowingFluid;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.AABB;
+import net.minecraftforge.client.ForgeHooksClient;
 import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
 import net.minecraftforge.common.ForgeMod;
 import org.joml.Matrix4f;
@@ -43,81 +47,169 @@ public class SteamEngineRenderer implements BlockEntityRenderer<SteamEngineBlock
 	public void render(SteamEngineBlockEntity blockEntity, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, int packedOverlay) {
 		int waterAmount = blockEntity.getWaterAmount();
 		if (waterAmount <= 0) return; // 如果没水就没有渲染的必要了
-		// 渲染一个透明小立方体，代替水
 		poseStack.pushPose();
-		// 使透明立方体居中并比透明容器略小
-		poseStack.translate(0.06252D, 0.5001D, 0.06252D);
-		poseStack.scale(0.8746F, 0.4365F * waterAmount / SteamEngineBlockEntity.MAX_WATER, 0.8746F);
+		renderWater((float) waterAmount / SteamEngineBlockEntity.MAX_WATER, blockEntity, poseStack, bufferSource, packedLight);
+		poseStack.popPose();
+	}
+
+	public static void renderWater(float waterAmount, BlockEntity blockEntity, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight) {
+		BlockPos pos = blockEntity.getBlockPos();
+		AABB box = blockEntity.getRenderBoundingBox();
+		poseStack.translate(-pos.getX(), -pos.getY(), -pos.getZ());
+		Matrix4f matrix4f = poseStack.last().pose();
+
+		float minX = (float) box.minX + 0.06252F;
+		float minY = (float) box.minY + 0.5F;
+		float minZ = (float) box.minZ + 0.06252F;
+		float maxX = (float) box.maxX - 0.06252F;
+		float maxY = (float) box.minY + 0.5F + 0.43748F * waterAmount;
+		float maxZ = (float) box.maxZ - 0.06252F;
+
+		FlowingFluid fluid = Fluids.WATER;
+		TextureAtlasSprite[] sprites = ForgeHooksClient.getFluidSprites(blockEntity.getLevel(), pos, fluid.defaultFluidState());
+		VertexConsumer buffer = bufferSource.getBuffer(ItemBlockRenderTypes.getRenderLayer(fluid.defaultFluidState()));
+		float u0 = sprites[0].getU(1.0D);
+		float u1 = sprites[0].getU(15.0D);
+		float v0 = sprites[0].getV(1.0D);
+		float v1 = sprites[0].getV(15.0D);
+		float u01 = sprites[1].getU(1.0D);
+		float u11 = sprites[1].getU(8.0D);
+		float v01 = sprites[1].getV(8.0D - 7.0D * waterAmount);
+		float v11 = sprites[1].getV(8.0D);
 		int fluidColor = IClientFluidTypeExtensions.of(ForgeMod.WATER_TYPE.get()).getTintColor(); // 获取水的颜色
 		float red = (float)(fluidColor >> 16 & 255) / 255.0F;
 		float green = (float)(fluidColor >> 8 & 255) / 255.0F;
 		float blue = (float)(fluidColor & 255) / 255.0F;
 		float alpha = (float)(fluidColor >> 24 & 255) / 255.0F;
-		// 执行渲染
-		this.main.render(poseStack, bufferSource.getBuffer(RenderType.entityTranslucent(TEXTURE)), packedLight, packedOverlay, red, green, blue, alpha);
-		poseStack.popPose();
 
-//		renderBoundingBox(poseStack, blockEntity);
-	}
+		// Up
+		buffer.vertex(matrix4f, minX, maxY, minZ)
+				.color(red, green, blue, alpha)
+				.uv(u0, v0)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
+		buffer.vertex(matrix4f, minX, maxY, maxZ)
+				.color(red, green, blue, alpha)
+				.uv(u0, v1)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
+		buffer.vertex(matrix4f, maxX, maxY, maxZ)
+				.color(red, green, blue, alpha)
+				.uv(u1, v1)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
+		buffer.vertex(matrix4f, maxX, maxY, minZ)
+				.color(red, green, blue, alpha)
+				.uv(u1, v0)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
 
-	public static void renderBoundingBox(PoseStack poseStack, SteamEngineBlockEntity blockEntity) { // 测试与学习顶点着色器用
-		AABB box = blockEntity.getRenderBoundingBox();
-		BlockPos pos = blockEntity.getBlockPos();
-		Tesselator tessellator = Tesselator.getInstance();
-		BufferBuilder buffer = tessellator.getBuilder();
-		RenderSystem.setShader(GameRenderer::getPositionColorShader);
-		buffer.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
-		RenderSystem.depthMask(false);
-		RenderSystem.enableBlend();
-		RenderSystem.defaultBlendFunc();
-		poseStack.pushPose();
+		// West
+		buffer.vertex(matrix4f, minX, maxY, minZ)
+				.color(red, green, blue, alpha)
+				.uv(u01, v01)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
+		buffer.vertex(matrix4f, minX, minY, minZ)
+				.color(red, green, blue, alpha)
+				.uv(u01, v11)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
+		buffer.vertex(matrix4f, minX, minY, maxZ)
+				.color(red, green, blue, alpha)
+				.uv(u11, v11)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
+		buffer.vertex(matrix4f, minX, maxY, maxZ)
+				.color(red, green, blue, alpha)
+				.uv(u11, v01)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
 
-		poseStack.translate(-pos.getX(), -pos.getY(), -pos.getZ());
+		// North
+		buffer.vertex(matrix4f, maxX, maxY, minZ)
+				.color(red, green, blue, alpha)
+				.uv(u01, v01)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
+		buffer.vertex(matrix4f, maxX, minY, minZ)
+				.color(red, green, blue, alpha)
+				.uv(u01, v11)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
+		buffer.vertex(matrix4f, minX, minY, minZ)
+				.color(red, green, blue, alpha)
+				.uv(u11, v11)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
+		buffer.vertex(matrix4f, minX, maxY, minZ)
+				.color(red, green, blue, alpha)
+				.uv(u11, v01)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
 
-		float minX = (float) box.minX;
-		float minY = (float) box.minY;
-		float minZ = (float) box.minZ;
-		float maxX = (float) box.maxX;
-		float maxY = (float) box.maxY;
-		float maxZ = (float) box.maxZ;
+		// South
+		buffer.vertex(matrix4f, minX, maxY, maxZ)
+				.color(red, green, blue, alpha)
+				.uv(u01, v01)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
+		buffer.vertex(matrix4f, minX, minY, maxZ)
+				.color(red, green, blue, alpha)
+				.uv(u01, v11)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
+		buffer.vertex(matrix4f, maxX, minY, maxZ)
+				.color(red, green, blue, alpha)
+				.uv(u11, v11)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
+		buffer.vertex(matrix4f, maxX, maxY, maxZ)
+				.color(red, green, blue, alpha)
+				.uv(u11, v01)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
 
-		Matrix4f mat = poseStack.last().pose();
-		float b = 1;
-		buffer.vertex(mat, minX, minY, minZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, minX, minY, maxZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, minX, maxY, maxZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, minX, maxY, minZ).color(0, 0, b, 0.5f).endVertex();
-
-		buffer.vertex(mat, maxX, minY, minZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, maxX, maxY, minZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, maxX, maxY, maxZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, maxX, minY, maxZ).color(0, 0, b, 0.5f).endVertex();
-
-		b = 0.5f;
-		buffer.vertex(mat, minX, minY, minZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, maxX, minY, minZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, maxX, minY, maxZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, minX, minY, maxZ).color(0, 0, b, 0.5f).endVertex();
-
-		b = 1;
-		buffer.vertex(mat, minX, maxY, minZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, minX, maxY, maxZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, maxX, maxY, maxZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, maxX, maxY, minZ).color(0, 0, b, 0.5f).endVertex();
-
-		b = 0.8f;
-		buffer.vertex(mat, minX, minY, minZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, minX, maxY, minZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, maxX, maxY, minZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, maxX, minY, minZ).color(0, 0, b, 0.5f).endVertex();
-
-		buffer.vertex(mat, minX, minY, maxZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, maxX, minY, maxZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, maxX, maxY, maxZ).color(0, 0, b, 0.5f).endVertex();
-		buffer.vertex(mat, minX, maxY, maxZ).color(0, 0, b, 0.5f).endVertex();
-
-		tessellator.end();
-		RenderSystem.depthMask(true);
-		poseStack.popPose();
+		// East
+		buffer.vertex(matrix4f, maxX, maxY, maxZ)
+				.color(red, green, blue, alpha)
+				.uv(u01, v01)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
+		buffer.vertex(matrix4f, maxX, minY, maxZ)
+				.color(red, green, blue, alpha)
+				.uv(u01, v11)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
+		buffer.vertex(matrix4f, maxX, minY, minZ)
+				.color(red, green, blue, alpha)
+				.uv(u11, v11)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
+		buffer.vertex(matrix4f, maxX, maxY, minZ)
+				.color(red, green, blue, alpha)
+				.uv(u11, v01)
+				.uv2(packedLight)
+				.normal(0.0F, 1.0F, 0.0F)
+				.endVertex();
 	}
 }

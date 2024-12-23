a5527da1e4e4b1e179399f0108c9e3268b56089f
vvvbbbcz <vvvbbbcz@bzgzs.cn>
2024-12-23 12:16:50 +0800

    refactor: y axis pipe addTick

--- src/api/java/net/industrybase/api/pipe/unit/StraightPipeY.java
+++ src/api/java/net/industrybase/api/pipe/unit/StraightPipeY.java
@@ -4,8 +4,6 @@ import net.industrybase.api.pipe.PipeNetwork;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 
-import java.util.ArrayDeque;
-
 public class StraightPipeY extends StraightPipe {
 	private int bottomAmount;
 
@@ -19,42 +17,38 @@ public class StraightPipeY extends StraightPipe {
 	}
 
 	@Override
-	public void setPressure(ArrayDeque<PipeUnit> tasks, Direction direction, double newPressure) {
-		if (direction.getAxis() != this.axis) return;
-		int i = direction.getAxisDirection().ordinal();
-		int j = (i == 0 ? 1 : 0);
-
-		double pressure = Math.max(newPressure, 0.0D);
-		this.tasks[i] = () -> {
-			this.pressures[i] = pressure;
-			if (this.neighbors[i] != null)
-				this.neighbors[i].onNeighborUpdatePressure(this, this.directions[j], pressure);
-		};
-
-		if (!this.submittedTask()) {
-			tasks.addLast(this);
-			this.setSubmittedTask();
-		}
-	}
-
-	@Override
 	public void addTick(Direction direction, double tick) {
 		if (tick > 0.0D) {
 			if (direction == Direction.UP) {
 				double diff = this.getMaxTick() - this.ticks[0];
 				if (tick > diff) tick = diff;
 				this.ticks[0] += tick;
+			}
+			this.ticks[1] = (double) (this.getMaxTick() * this.bottomAmount) / this.getCapacity();
+
+			double amountPressure = (double) (this.size() * this.bottomAmount) / this.getCapacity();
+
+			if (this.fullTick() || this.full()) {
+				this.ticks[0] = 0.0D;
+				if (this.neighbors[0] != null) {
+					this.setPressure(this.network.getTask(), this.directions[1], this.neighborPressures[0] + amountPressure);
+				} else { // rebound pressure
+					this.setPressure(this.network.getTask(), this.directions[1], Math.max(this.neighborPressures[1], amountPressure));
+				}
 			} else {
-				this.setPressure(this.network.getTask(), this.directions[1], (double) (this.size() * this.bottomAmount) / this.getCapacity());
+				this.setPressure(this.network.getTask(), this.directions[1], amountPressure);
 			}
-			if (this.fullTick()) {
-				this.setPressure(this.network.getTask(), this.directions[1], this.neighborPressures[0] + (double) (this.size() * this.bottomAmount) / this.getCapacity());
-				this.ticks[0] = this.getMaxTick() - this.ticks[1];
-			}
+
 			if (this.full()) {
-				this.setPressure(this.network.getTask(), this.directions[0], this.neighborPressures[1] - (double) (this.size() * this.amount) / this.getCapacity());
 				this.ticks[0] = 0.0D;
 				this.ticks[1] = this.getMaxTick();
+				if (this.neighbors[1] != null) {
+					this.setPressure(this.network.getTask(), this.directions[0], this.neighborPressures[1] - amountPressure);
+				} else {
+					this.setPressure(this.network.getTask(), this.directions[0], this.neighborPressures[0]);
+				}
+			} else {
+				this.setPressure(this.network.getTask(), this.directions[0], 0.0D);
 			}
 		}
 	}
@@ -66,7 +60,6 @@ public class StraightPipeY extends StraightPipe {
 			if (direction == Direction.DOWN || this.fullTick()) {
 				this.bottomAmount += result;
 				if (this.bottomAmount < 0) this.bottomAmount = 0;
-				this.ticks[1] = (double) (this.getMaxTick() * this.bottomAmount) / this.getCapacity();
 			}
 		}
 		return result;

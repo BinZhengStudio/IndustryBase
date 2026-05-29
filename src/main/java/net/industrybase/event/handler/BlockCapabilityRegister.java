215f48940bf60a75f5dac2f5679fe640ee0429bb
vvvbbbcz <ms_vvvbbbcz@mail.scut.edu.cn>
2026-05-29 23:14:04 +0800

    trans: block capability register

--- src/main/java/net/industrybase/event/handler/BlockCapabilityRegister.java
+++ src/main/java/net/industrybase/event/handler/BlockCapabilityRegister.java
@@ -2,35 +2,17 @@ package net.industrybase.event.handler;
 
 import net.industrybase.api.CapabilityList;
 import net.industrybase.api.IndustryBaseApi;
-import net.industrybase.api.transmit.TransmissionRodBlockEntity;
 import net.industrybase.world.level.block.entity.*;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
-import net.neoforged.neoforge.registries.DeferredHolder;
 
-@EventBusSubscriber(modid = IndustryBaseApi.MODID, bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = IndustryBaseApi.MODID)
 public class BlockCapabilityRegister {
 	@SubscribeEvent
 	private static void registerCapabilities(final RegisterCapabilitiesEvent event) {
-		// transmission rods
-		registerTransmissionRod(event, BlockEntityTypeList.OAK_TRANSMISSION_ROD);
-		registerTransmissionRod(event, BlockEntityTypeList.DARK_OAK_TRANSMISSION_ROD);
-		registerTransmissionRod(event, BlockEntityTypeList.ACACIA_TRANSMISSION_ROD);
-		registerTransmissionRod(event, BlockEntityTypeList.BIRCH_TRANSMISSION_ROD);
-		registerTransmissionRod(event, BlockEntityTypeList.JUNGLE_TRANSMISSION_ROD);
-		registerTransmissionRod(event, BlockEntityTypeList.SPRUCE_TRANSMISSION_ROD);
-		registerTransmissionRod(event, BlockEntityTypeList.MANGROVE_TRANSMISSION_ROD);
-		registerTransmissionRod(event, BlockEntityTypeList.CHERRY_TRANSMISSION_ROD);
-		registerTransmissionRod(event, BlockEntityTypeList.CRIMSON_TRANSMISSION_ROD);
-		registerTransmissionRod(event, BlockEntityTypeList.WARPED_TRANSMISSION_ROD);
-		registerTransmissionRod(event, BlockEntityTypeList.STONE_TRANSMISSION_ROD);
-		registerTransmissionRod(event, BlockEntityTypeList.IRON_TRANSMISSION_ROD);
-		registerTransmissionRod(event, BlockEntityTypeList.GOLD_TRANSMISSION_ROD);
-		registerTransmissionRod(event, BlockEntityTypeList.DIAMOND_TRANSMISSION_ROD);
-
+        event.registerBlockEntity(CapabilityList.MECHANICAL_TRANSMIT, BlockEntityTypeList.TRANSMISSION_ROD.get(), TransmissionRodBlockEntity::getTransmit);
 		event.registerBlockEntity(CapabilityList.MECHANICAL_TRANSMIT, BlockEntityTypeList.ELECTRIC_MOTOR.get(), ElectricMotorBlockEntity::getTransmit);
 		event.registerBlockEntity(CapabilityList.MECHANICAL_TRANSMIT, BlockEntityTypeList.CREATIVE_ELECTRIC_MOTOR.get(), CreativeElectricMotorBlockEntity::getTransmit);
 		event.registerBlockEntity(CapabilityList.MECHANICAL_TRANSMIT, BlockEntityTypeList.DYNAMO.get(), DynamoBlockEntity::getTransmit);
@@ -46,23 +28,15 @@ public class BlockCapabilityRegister {
 		event.registerBlockEntity(CapabilityList.ELECTRIC_POWER, BlockEntityTypeList.DYNAMO.get(), DynamoBlockEntity::getElectricPower);
 		event.registerBlockEntity(CapabilityList.ELECTRIC_POWER, BlockEntityTypeList.ELECTRIC_MOTOR.get(), ElectricMotorBlockEntity::getElectricPower);
 
-		event.registerBlockEntity(Capabilities.EnergyStorage.BLOCK, BlockEntityTypeList.WIRE_CONNECTOR.get(), WireConnectorBlockEntity::getElectricPower);
-		event.registerBlockEntity(Capabilities.EnergyStorage.BLOCK, BlockEntityTypeList.WIRE.get(), WireBlockEntity::getElectricPower);
-		event.registerBlockEntity(Capabilities.EnergyStorage.BLOCK, BlockEntityTypeList.CREATIVE_DYNAMO.get(), CreativeDynamoBlockEntity::getElectricPower);
-		event.registerBlockEntity(Capabilities.EnergyStorage.BLOCK, BlockEntityTypeList.CREATIVE_ELECTRIC_MOTOR.get(), CreativeElectricMotorBlockEntity::getElectricPower);
-		event.registerBlockEntity(Capabilities.EnergyStorage.BLOCK, BlockEntityTypeList.DYNAMO.get(), DynamoBlockEntity::getElectricPower);
-		event.registerBlockEntity(Capabilities.EnergyStorage.BLOCK, BlockEntityTypeList.ELECTRIC_MOTOR.get(), ElectricMotorBlockEntity::getElectricPower);
+		event.registerBlockEntity(Capabilities.Energy.BLOCK, BlockEntityTypeList.WIRE_CONNECTOR.get(), WireConnectorBlockEntity::getElectricPower);
+		event.registerBlockEntity(Capabilities.Energy.BLOCK, BlockEntityTypeList.WIRE.get(), WireBlockEntity::getElectricPower);
+		event.registerBlockEntity(Capabilities.Energy.BLOCK, BlockEntityTypeList.CREATIVE_DYNAMO.get(), CreativeDynamoBlockEntity::getElectricPower);
+		event.registerBlockEntity(Capabilities.Energy.BLOCK, BlockEntityTypeList.CREATIVE_ELECTRIC_MOTOR.get(), CreativeElectricMotorBlockEntity::getElectricPower);
+		event.registerBlockEntity(Capabilities.Energy.BLOCK, BlockEntityTypeList.DYNAMO.get(), DynamoBlockEntity::getElectricPower);
+		event.registerBlockEntity(Capabilities.Energy.BLOCK, BlockEntityTypeList.ELECTRIC_MOTOR.get(), ElectricMotorBlockEntity::getElectricPower);
 
-		event.registerBlockEntity(Capabilities.FluidHandler.BLOCK, BlockEntityTypeList.STEAM_ENGINE.get(), SteamEngineBlockEntity::getTank);
-		event.registerBlockEntity(Capabilities.FluidHandler.BLOCK, BlockEntityTypeList.CREATIVE_STEAM_ENGINE.get(), CreativeSteamEngineBlockEntity::getTank);
-		event.registerBlockEntity(Capabilities.FluidHandler.BLOCK, BlockEntityTypeList.FLUID_TANK.get(), FluidTankBlockEntity::getTank);
-	}
-
-	private static <T extends TransmissionRodBlockEntity> void registerTransmissionRod(RegisterCapabilitiesEvent event,
-																					   DeferredHolder<BlockEntityType<?>, BlockEntityType<T>> holder) {
-		event.registerBlockEntity(
-				CapabilityList.MECHANICAL_TRANSMIT,
-				holder.get(),
-				TransmissionRodBlockEntity::getTransmit);
+		event.registerBlockEntity(Capabilities.Fluid.BLOCK, BlockEntityTypeList.STEAM_ENGINE.get(), SteamEngineBlockEntity::getTank);
+		event.registerBlockEntity(Capabilities.Fluid.BLOCK, BlockEntityTypeList.CREATIVE_STEAM_ENGINE.get(), CreativeSteamEngineBlockEntity::getTank);
+		event.registerBlockEntity(Capabilities.Fluid.BLOCK, BlockEntityTypeList.FLUID_TANK.get(), FluidTankBlockEntity::getTank);
 	}
 }

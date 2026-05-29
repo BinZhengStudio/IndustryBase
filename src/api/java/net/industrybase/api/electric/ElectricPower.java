0d9b2c80e27ec35a2cbad3c69bdd50d600586c04
vvvbbbcz <ms_vvvbbbcz@mail.scut.edu.cn>
2026-05-29 22:51:33 +0800

    trans: electric api

--- src/api/java/net/industrybase/api/electric/ElectricPower.java
+++ src/api/java/net/industrybase/api/electric/ElectricPower.java
@@ -1,23 +1,28 @@
 package net.industrybase.api.electric;
 
-import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import net.industrybase.api.energy.IElectricPower;
 import net.industrybase.api.network.client.UnsubscribeWireConnPacket;
 import net.industrybase.api.util.NbtHelper;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.*;
+import net.minecraft.util.ExtraCodecs;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.neoforged.neoforge.common.util.INBTSerializable;
-import net.neoforged.neoforge.energy.IEnergyStorage;
-import net.neoforged.neoforge.network.PacketDistributor;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
+import net.neoforged.neoforge.client.network.ClientPacketDistributor;
+import net.neoforged.neoforge.common.util.ValueIOSerializable;
+import net.neoforged.neoforge.transfer.TransferPreconditions;
+import net.neoforged.neoforge.transfer.energy.EnergyHandler;
+import net.neoforged.neoforge.transfer.transaction.SnapshotJournal;
+import net.neoforged.neoforge.transfer.transaction.TransactionContext;
+
+import org.jspecify.annotations.Nullable;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class ElectricPower implements IElectricPower, IEnergyStorage, INBTSerializable<CompoundTag> {
+public class ElectricPower implements IElectricPower, EnergyHandler, ValueIOSerializable {
 	private double tmpOutputPower;
 	private double tmpInputPower;
 	private final Set<BlockPos> tmpConn;
@@ -27,6 +32,8 @@ public class ElectricPower implements IElectricPower, IEnergyStorage, INBTSerial
 	private final BlockEntity blockEntity;
 	@Nullable
 	private ElectricNetwork network;
+    private final EnergyJournal energyJournal = new EnergyJournal();
+    private int tmpFE = 0;
 
 	public ElectricPower(BlockEntity blockEntity) {
 		this.blockEntity = blockEntity;
@@ -42,7 +49,7 @@ public class ElectricPower implements IElectricPower, IEnergyStorage, INBTSerial
 		this.level = this.blockEntity.getLevel();
 		if (this.level != null) {
 			this.network = ElectricNetwork.Manager.get(this.level);
-			if (!this.level.isClientSide) {
+			if (!this.level.isClientSide()) {
 				this.setOutputPower(this.tmpOutputPower);
 				this.setInputPower(this.tmpInputPower);
 				this.network.addOrChangeBlock(this.pos, this.blockEntity::setChanged);
@@ -59,8 +66,8 @@ public class ElectricPower implements IElectricPower, IEnergyStorage, INBTSerial
 	public void remove() {
 		if (this.level != null) {
 			if (this.network != null) {
-				if (level.isClientSide) {
-					PacketDistributor.sendToServer(new UnsubscribeWireConnPacket(this.pos));
+				if (level.isClientSide()) {
+					ClientPacketDistributor.sendToServer(new UnsubscribeWireConnPacket(this.pos));
 					this.network.removeClientWires(this.pos);
 				} else {
 					this.network.removeBlock(this.pos, this.blockEntity::setChanged);
@@ -87,7 +94,7 @@ public class ElectricPower implements IElectricPower, IEnergyStorage, INBTSerial
 	 */
 	@Override
 	public double setOutputPower(double power) {
-		if (!this.level.isClientSide) {
+		if (!this.level.isClientSide()) {
 			double diff = this.network.setMachineOutput(this.pos, power);
 			if (diff != 0) this.blockEntity.setChanged();
 			return diff;
@@ -107,7 +114,7 @@ public class ElectricPower implements IElectricPower, IEnergyStorage, INBTSerial
 
 	@Override
 	public double setInputPower(double power) {
-		if (!this.level.isClientSide) {
+		if (!this.level.isClientSide()) {
 			double diff = this.network.setMachineInput(this.pos, power);
 			if (diff != 0) this.blockEntity.setChanged();
 			return diff;
@@ -125,71 +132,82 @@ public class ElectricPower implements IElectricPower, IEnergyStorage, INBTSerial
 		return this.network.getRealInput(this.pos);
 	}
 
-	@Override
-	public CompoundTag serializeNBT(@Nullable HolderLookup.Provider provider) {
-		CompoundTag nbt = new CompoundTag();
-		ListTag listTag = new ListTag();
-		nbt.putDouble("Output", this.getOutputPower());
-		nbt.putDouble("Input", this.getInputPower());
-		this.network.getWireConn(this.pos).forEach(pos -> listTag.add(NbtUtils.writeBlockPos(pos)));
-		nbt.put("Connections", listTag);
-		return nbt;
-	}
+    @Override
+    public void serialize(ValueOutput output) {
+        output.putDouble("Output", this.getOutputPower());
+        output.putDouble("Input", this.getInputPower());
 
-	@Override
-	public void deserializeNBT(@Nullable HolderLookup.Provider provider, CompoundTag nbt) {
-		this.tmpOutputPower = nbt.getDouble("Output");
-		this.tmpInputPower = nbt.getDouble("Input");
-		nbt.getList("Connections", Tag.TAG_INT_ARRAY).forEach(entry ->
-				NbtHelper.readBlockPos((IntArrayTag) entry).ifPresent(this.tmpConn::add));
-	}
+        var list = output.list("Connections", ExtraCodecs.NBT);
+        this.network.getWireConn(this.pos).forEach(pos -> list.add(NbtHelper.writeBlockPos(pos)));
+    }
 
-	public void readFromNBT(CompoundTag tag) {
-		CompoundTag nbt = tag.getCompound("ElectricPower");
-		this.deserializeNBT(null, nbt);
-	}
+    @Override
+    public void deserialize(ValueInput input) {
+        this.tmpOutputPower = input.getDoubleOr("Output", 0.0);
+        this.tmpInputPower = input.getDoubleOr("Input", 0.0);
 
-	@CanIgnoreReturnValue
-	public CompoundTag writeToNBT(CompoundTag tag) {
-		CompoundTag nbt = new CompoundTag();
-		ListTag listTag = new ListTag();
-		if (this.network != null) {
-			nbt.putDouble("Output", this.getOutputPower());
-			nbt.putDouble("Input", this.getInputPower());
-			this.network.getWireConn(this.pos).forEach(pos -> listTag.add(NbtUtils.writeBlockPos(pos)));
-			nbt.put("Connections", listTag);
-		}
-		tag.put("ElectricPower", nbt);
-		return tag;
-	}
+        input.listOrEmpty("Connections", ExtraCodecs.NBT).forEach(entry -> {
+            if (entry instanceof IntArrayTag tag)
+                NbtHelper.readBlockPos(tag).ifPresent(this.tmpConn::add);
+        });
+    }
 
-	@Override
-	public int receiveEnergy(int maxReceive, boolean simulate) {
-		return this.network.receiveFEEnergy(this.pos, maxReceive, simulate);
-	}
+    @Override
+    public long getAmountAsLong() {
+        return this.network.getFEEnergy(this.pos);
+    }
 
-	@Override
-	public int extractEnergy(int maxExtract, boolean simulate) {
-		return this.network.extractFEEnergy(this.pos, maxExtract, simulate);
-	}
+    @Override
+    public long getCapacityAsLong() {
+        return this.network.getMaxFEStored(this.pos);
+    }
 
-	@Override
-	public int getEnergyStored() {
-		return this.network.getFEEnergy(this.pos);
-	}
+    @Override
+    public int insert(int amount, TransactionContext transaction) {
+        TransferPreconditions.checkNonNegative(amount);
 
-	@Override
-	public int getMaxEnergyStored() {
-		return this.network.getMaxFEStored(this.pos);
-	}
+        int inserted = Math.min(this.getCapacityAsInt() - this.getAmountAsInt(), amount);
+        if (inserted > 0) {
+            energyJournal.updateSnapshots(transaction);
+            tmpFE += inserted;
+            return inserted;
+        }
+        return 0;
+    }
 
-	@Override
-	public boolean canExtract() {
-		return this.network.getFEEnergy(this.pos) > 0;
-	}
+    @Override
+    public int extract(int amount, TransactionContext transaction) {
+        TransferPreconditions.checkNonNegative(amount);
 
-	@Override
-	public boolean canReceive() {
-		return this.network.getFEEnergy(this.pos) < this.getMaxEnergyStored();
-	}
+        int extracted = Math.min(this.getAmountAsInt(), amount);
+        if (extracted > 0) {
+            energyJournal.updateSnapshots(transaction);
+            tmpFE -= extracted;
+            return extracted;
+        }
+        return 0;
+    }
+
+    private class EnergyJournal extends SnapshotJournal<Integer> {
+        @Override
+        protected Integer createSnapshot() {
+            return tmpFE;
+        }
+
+        @Override
+        protected void revertToSnapshot(Integer snapshot) {
+            tmpFE = snapshot;
+        }
+
+        @Override
+        protected void onRootCommit(Integer originalState) {
+            int previousAmount = originalState;
+            if (tmpFE > previousAmount) {
+                network.receiveFEEnergy(pos, tmpFE - previousAmount, false);
+            } else if (tmpFE < previousAmount) {
+                network.extractFEEnergy(pos, previousAmount - tmpFE, false);
+            }
+            tmpFE = 0;
+        }
+    }
 }

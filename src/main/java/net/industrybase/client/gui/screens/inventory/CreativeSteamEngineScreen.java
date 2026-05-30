10ef94d6f5ea9db1fcc1a3d5d645dd9674d4d4a8
vvvbbbcz <ms_vvvbbbcz@mail.scut.edu.cn>
2026-05-30 16:35:34 +0800

    trans & refactor: steam engine screen

--- src/main/java/net/industrybase/client/gui/screens/inventory/CreativeSteamEngineScreen.java
+++ src/main/java/net/industrybase/client/gui/screens/inventory/CreativeSteamEngineScreen.java
@@ -1,45 +1,39 @@
 package net.industrybase.client.gui.screens.inventory;
 
 import net.industrybase.api.IndustryBaseApi;
-import net.industrybase.api.util.TransmitScreenHelper;
 import net.industrybase.world.inventory.CreativeSteamEngineMenu;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.player.Inventory;
 
 public class CreativeSteamEngineScreen extends AbstractContainerScreen<CreativeSteamEngineMenu> {
-	private static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(IndustryBaseApi.MODID, "textures/gui/container/steam_engine.png");
+    private static final Identifier TEXTURE = Identifier.fromNamespaceAndPath(IndustryBaseApi.MODID,
+            "textures/gui/container/steam_engine.png");
 
-	public CreativeSteamEngineScreen(CreativeSteamEngineMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
-		super(pMenu, pPlayerInventory, pTitle);
-	}
+    public CreativeSteamEngineScreen(CreativeSteamEngineMenu menu, Inventory playerInventory, Component title) {
+        super(menu, playerInventory, title);
+    }
 
-	@Override
-	protected void init() {
-		super.init();
-		this.leftPos -= TransmitScreenHelper.PANEL_WIDTH / 2;
-	}
+    @Override
+    public void extractBackground(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float a) {
+        super.extractBackground(graphics, mouseX, mouseY, a);
+        graphics.blit(RenderPipelines.GUI_TEXTURED, TEXTURE, this.leftPos, this.topPos, 0.0F, 0.0F, this.imageWidth,
+                this.imageHeight, 256, 256);
+        // 渲染水量指示
+        graphics.blit(RenderPipelines.GUI_TEXTURED, TEXTURE, this.leftPos + 149, this.topPos + 9, 176, 0, 17, 65, 256,
+                256);
+        // 渲染燃料燃烧进度
+        graphics.blit(RenderPipelines.GUI_TEXTURED, TEXTURE, this.leftPos + 70, this.topPos + 24, 176, 66, 14, 14, 256,
+                256);
+    }
 
-	@Override
-	public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
-		super.render(graphics, mouseX, mouseY, partialTicks);
-		// 渲染水量的文字
-		graphics.drawString(this.font, Component.translatable("label.steam_engine.water_amount", 2000), this.leftPos + 8, this.topPos + 39, 0x006ee4, false);
-		// 渲染信息面板的内容
-		TransmitScreenHelper.renderTransmitFont(graphics, this.font, this.menu.getData().get(0), 100, this.leftPos, this.topPos, this.imageWidth);
-		this.renderTooltip(graphics, mouseX, mouseY);
-	}
-
-	@Override
-	protected void renderBg(GuiGraphics graphics, float partialTick, int mouseX, int mouseY) {
-		graphics.blit(TEXTURE, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
-		// 渲染水量指示
-		graphics.blit(TEXTURE, this.leftPos + 149, this.topPos + 9, 176, 0, 17, 65);
-		// 渲染燃料燃烧进度
-		graphics.blit(TEXTURE, this.leftPos + 70, this.topPos + 24, 176, 66, 14, 14);
-		// 在右侧渲染一个用于显示传动网络信息的面板
-		TransmitScreenHelper.renderTransmitPanel(graphics, this.leftPos, this.topPos, this.imageWidth);
-	}
+    @Override
+    protected void extractLabels(GuiGraphicsExtractor graphics, int xm, int ym) {
+        super.extractLabels(graphics, xm, ym);
+        graphics.text(this.font, Component.translatable("label.steam_engine.water_amount", 2000), this.leftPos + 8,
+                this.topPos + 39, 0x006ee4, false);
+    }
 }
--- src/main/java/net/industrybase/client/gui/screens/inventory/SteamEngineScreen.java
+++ src/main/java/net/industrybase/client/gui/screens/inventory/SteamEngineScreen.java
@@ -1,53 +1,49 @@
 package net.industrybase.client.gui.screens.inventory;
 
 import net.industrybase.api.IndustryBaseApi;
-import net.industrybase.api.util.TransmitScreenHelper;
 import net.industrybase.world.inventory.SteamEngineMenu;
 import net.industrybase.world.level.block.entity.SteamEngineBlockEntity;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.player.Inventory;
 
 public class SteamEngineScreen extends AbstractContainerScreen<SteamEngineMenu> {
-	private static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(IndustryBaseApi.MODID, "textures/gui/container/steam_engine.png");
+    private static final Identifier TEXTURE = Identifier.fromNamespaceAndPath(IndustryBaseApi.MODID,
+            "textures/gui/container/steam_engine.png");
 
-	public SteamEngineScreen(SteamEngineMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
-		super(pMenu, pPlayerInventory, pTitle);
-	}
+    public SteamEngineScreen(SteamEngineMenu menu, Inventory playerInventory, Component title) {
+        super(menu, playerInventory, title);
+    }
 
-	@Override
-	protected void init() {
-		super.init();
-		this.leftPos -= TransmitScreenHelper.PANEL_WIDTH / 2;
-	}
+    @Override
+    public void extractBackground(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float a) {
+        super.extractBackground(graphics, mouseX, mouseY, a);
+        graphics.blit(RenderPipelines.GUI_TEXTURED, TEXTURE, this.leftPos, this.topPos, 0.0F, 0.0F, this.imageWidth,
+                this.imageHeight, 256, 256);
 
-	@Override
-	public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
-		super.render(graphics, mouseX, mouseY, partialTicks);
-		// 渲染水量的文字
-		graphics.drawString(this.font, Component.translatable("label.steam_engine.water_amount", this.menu.getData().get(4)), this.leftPos + 8, this.topPos + 39, 0x006ee4, false);
-		// 渲染信息面板的内容
-		TransmitScreenHelper.renderTransmitFont(graphics, this.font, this.menu.getData().get(1), this.menu.getData().get(0), this.leftPos, this.topPos, this.imageWidth);
-		this.renderTooltip(graphics, mouseX, mouseY);
-	}
+        int waterAmount = this.menu.getData().get(4);
+        if (waterAmount > 0) { // 渲染水量指示
+            int textureHeight = (int) (64.0F * waterAmount / SteamEngineBlockEntity.MAX_WATER);
+            graphics.blit(RenderPipelines.GUI_TEXTURED, TEXTURE, this.leftPos + 149, this.topPos + 73 - textureHeight,
+                    176, 64 - textureHeight, 17, textureHeight + 1, 256, 256);
+        }
 
-	@Override
-	protected void renderBg(GuiGraphics graphics, float partialTick, int mouseX, int mouseY) {
-		graphics.blit(TEXTURE, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
-		int waterAmount = this.menu.getData().get(4);
-		if (waterAmount > 0) { // 渲染水量指示
-			int textureHeight = (int) (64.0F * waterAmount / SteamEngineBlockEntity.MAX_WATER);
-			graphics.blit(TEXTURE, this.leftPos + 149, this.topPos + 73 - textureHeight, 176, 64 - textureHeight, 17, textureHeight + 1);
-		}
-		int burnTime = this.menu.getData().get(2);
-		if (burnTime > 0) { // 渲染燃料燃烧进度
-			int totalBurnTime = this.menu.getData().get(3) > 0 ? this.menu.getData().get(3) : 200;
-			int textureHeight = (int) (13.0F * burnTime / totalBurnTime);
-			graphics.blit(TEXTURE, this.leftPos + 70, this.topPos + 37 - textureHeight, 176, 79 - textureHeight, 14, textureHeight + 1);
-		}
-		// 在右侧渲染一个用于显示传动网络信息的面板
-		TransmitScreenHelper.renderTransmitPanel(graphics, this.leftPos, this.topPos, this.imageWidth);
-	}
+        int burnTime = this.menu.getData().get(2);
+        if (burnTime > 0) { // 渲染燃料燃烧进度
+            int totalBurnTime = this.menu.getData().get(3) > 0 ? this.menu.getData().get(3) : 200;
+            int textureHeight = (int) (13.0F * burnTime / totalBurnTime);
+            graphics.blit(RenderPipelines.GUI_TEXTURED, TEXTURE, this.leftPos + 70, this.topPos + 37 - textureHeight,
+                    176, 79 - textureHeight, 14, textureHeight + 1, 256, 256);
+        }
+    }
+
+    @Override
+    protected void extractLabels(GuiGraphicsExtractor graphics, int xm, int ym) {
+        super.extractLabels(graphics, xm, ym);
+        graphics.text(this.font, Component.translatable("label.steam_engine.water_amount", this.menu.getData().get(4)),
+                this.leftPos + 8, this.topPos + 39, 0x006ee4, false);
+    }
 }
